b7e438007083a3c6459fe5da94cb9fc5f7675a82
AndreyKapus <kapustnikov@ukr.net>
2022-08-18 13:07:27 +0300

    test

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,16 +1,9 @@
-function fetchCountry(name) {
-  const URL = 'https://restcountries.com/v3.1/name/';
-
-  fetch(`${URL}/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
-    .catch(error => {
-      console.log('ошибка!');
-    });
+export function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages
+`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
 }
-
-export { fetchCountry };
--- src/index.js
+++ src/index.js
@@ -1,45 +1,39 @@
 import './css/styles.css';
 var debounce = require('lodash.debounce');
-import { fetchCountry } from './fetchCountries';
+import { fetchCountries } from './fetchCountries';
 var debounce = require('lodash.debounce');
-
 const DEBOUNCE_DELAY = 300;
 
 const input = document.getElementById('search-box');
 const countryList = document.querySelector('.country-list');
-
-input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
-
 function onSearch(e) {
   e.preventDefault();
-  const form = e.currentTarget;
-  const searchInput = input.value;
-
-  fetchCountry(searchInput)
-    .then(renderCountry)
-    .catch(error => {
-      console.log('error');
-    });
+  // const form = e.currentTarget;
+  const searchInput = input.value.trim();
+  fetchCountries(searchInput).then(checkData).catch(showError);
 }
-
 //  Функции для markup -----------//
-
 function createInfoMarkup() {
   return `<li>
   ${'flags.svg'} ${'name'}
 </li>`;
 }
-
+function checkData(data) {
+  if (data.length > 10) {
+    console.log('Too many matches found. Please enter a more specific name.');
+  } else if (data.length === 0) {
+    console.log('Oops, there is no country with that name');
+  } else if (data.length >= 2 && data.length <= 10) {
+    console.log('render list');
+  } else if (data.length === 1) {
+    console.log('render 1 country');
+  }
+}
 function renderCountry(country) {
   const markup = createInfoMarkup(country);
   countryList.insertAdjacentHTML('beforebegin', createInfoMarkup());
 }
-
-// function renderCountry(country) {
-//   const markup = createInfoMarkup(country).map(country => {
-//     return `<li>
-//     ${'flags.svg'} ${'name'}
-//   </li>`;
-//   });
-//   countryList.insertAdjacentHTML('beforebegin', markup);
-// }
+function showError(error) {
+  console.log(error);
+}
+input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
